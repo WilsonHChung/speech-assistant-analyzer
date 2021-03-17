81914a8af8b2c02b489f390a11fb4a949e861ff4
Wilson Chung <wilsonhchung@gmail.com>
2021-03-16 21:03:54 -0700

    MImplemented basic highlighting functionality for filler words

--- Makefile
+++ Makefile
@@ -6,3 +6,7 @@ delete app:
 	docker stop speech-assistant-analyzer_app_1
 	docker rm speech-assistant-analyzer_app_1
 	docker image rm speech-assistant-analyzer_app:latest
+format:
+	docker stop speech-assistant-analyzer_app_1 speech-assistant-analyzer_db_1
+	docker rm speech-assistant-analyzer_app_1 speech-assistant-analyzer_db_1
+	docker image rm speech-assistant-analyzer_app:latest mysql:8.0.19
--- app/src/app.py
+++ app/src/app.py
@@ -16,6 +16,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 sql_hostname = "localhost"
 sql_port = 3306
 
+
 def connect_to_db(hostname, port):
     connected = False
     
@@ -40,45 +41,44 @@ def connect_to_db(hostname, port):
 
     return None
 
+def display_transcript():
 
-# the home page now can upload and post function
-@app.route("/", methods=["GET", "POST"])
-def record():
-
-    cnx = connect_to_db(sql_hostname, sql_port)
-    cursor = cnx.cursor()
-
-    cursor.execute("SHOW tables")
-    cursor.execute("CREATE TABLE hello (the_date DATE)")
-    cnx.commit()
-
-    authenticator = IAMAuthenticator('AeWcu0gUgQQwm8PKD7knwKGXxVYuqYp9kRb9qH96-ZBs')
+    authenticator = IAMAuthenticator('')
     service = SpeechToTextV1(authenticator=authenticator)
     service.set_service_url('https://stream.watsonplatform.net/speech-to-text/api')
 
     models = service.list_models().get_result()
-
     model = service.get_model('en-US_BroadbandModel').get_result()
+
     output = ""
+    record = ""
     index = 0
     index_limit = False
-    record = ""
+
     with open(join(dirname(__file__), 'hesitation.wav'),
           'rb') as audio_file:
         output = service.recognize(
         audio=audio_file,
         content_type='audio/wav').get_result()
-    # print(json.dumps(output, indent=2))
-    # filters the JSON dump to print out only the transcript 
+
     while index_limit == False:
         try:
             record += output['results'][index]['alternatives'][0]['transcript']
-            # print(output['results'][index]['alternatives'][0]['transcript'], end = ' ')
             index += 1
         except IndexError:
             index_limit = True
             break
-    return render_template('base.html',record = record)
+
+    return record
+
+
+# the home page now can upload and post function
+@app.route("/", methods=["GET", "POST"])
+def record():
+
+    transcript = display_transcript()
+
+    return render_template('base.html',record = transcript)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
