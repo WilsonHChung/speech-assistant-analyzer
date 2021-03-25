10517fa1149777d63a81cf001ee34198ce0c1fce
Thai Truong <akita.thai94@gmail.com>
2021-03-24 17:05:01 -0700

    Update speech_processing that works

--- speech_processing.py
+++ speech_processing.py
@@ -1,49 +1,52 @@
-import json
+import pyaudio
+import config
 from os.path import join, dirname
 from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
-import threading
+from threading import Thread
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import pandas as pd
 
-authenticator = IAMAuthenticator('ukQ17BPHEoBP0tBYovtuQn_Dlb5dfH4waoA5Zpy_w8aO')
-service = SpeechToTextV1(authenticator=authenticator)
-service.set_service_url('https://stream.watsonplatform.net/speech-to-text/api')
-
-models = service.list_models().get_result()
-
-model = service.get_model('en-US_BroadbandModel').get_result()
-
-output = ""
-index = 0
-index_limit = False
-
-# inputs the audio file into the IBM Watson Cloud service to figure out speech to text 
-
-with open(join(dirname(__file__), 'hesitation.wav'),
-          'rb') as audio_file:
-    output = service.recognize(
-            audio=audio_file,
-            content_type='audio/wav').get_result()
-    # print(json.dumps(output, indent=2))
-
-# filters the JSON dump to print out only the transcript 
-
-while index_limit == False:
-    try:
-        print(output['results'][index]['alternatives'][0]['transcript'], end = ' ')
-        index += 1
-    except IndexError:
-        index_limit = True
-        break
+authenticator = IAMAuthenticator(config.api_key)
+speech_to_text = SpeechToTextV1(authenticator=authenticator)
 
 
+# AudioSource input params will be initialized as queue when using microphone. 
+# The pyaudio stream would be continously adding recordings to the queue, then websocket client would be
+# sending the recordings to the speech to text service. 
+
+try:
+    from Queue import Queue, Full
+except ImportError:
+    from queue import Queue, Full
+
+###############################################
+#### Initalize queue to store the recordings ##
+###############################################
+CHUNK = 1024
+# Note: It will discard if the websocket client can't consumme fast enough
+# So, increase the max size as per your choice
+BUF_MAX_SIZE = CHUNK * 10
+# Buffer to store audio
+q = Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK)))
+
+# Create an instance of AudioSource
+audio_source = AudioSource(q, True, True)
+
+###############################################
+#### Prepare Speech to Text Service ########
+###############################################
+
+# initialize speech to text service
+authenticator = IAMAuthenticator(config.api_key)
+speech_to_text = SpeechToTextV1(authenticator=authenticator)
+
+# define callback for the speech to text service
 class MyRecognizeCallback(RecognizeCallback):
     def __init__(self):
         RecognizeCallback.__init__(self)
 
     def on_transcription(self, transcript):
-        print(transcript)
+        print(transcript[0]['transcript'])
 
     def on_connected(self):
         print('Connection was successful')
@@ -57,9 +60,70 @@ class MyRecognizeCallback(RecognizeCallback):
     def on_listening(self):
         print('Service is listening')
 
-    def on_hypothesis(self, hypothesis):
-        print(hypothesis)
+    # def on_hypothesis(self, hypothesis):
+    #     print(hypothesis)
 
-    def on_data(self, data):
-        print(data)
+    # def on_data(self, data):
+    #     print(data)
 
+    def on_close(self):
+        print("Connection closed")
+
+# this function will initiate the recognize service and pass in the AudioSource
+def recognize_using_weboscket(*args):
+    mycallback = MyRecognizeCallback()
+    speech_to_text.recognize_using_websocket(audio=audio_source,
+                                             content_type='audio/l16; rate=44100',
+                                             recognize_callback=mycallback,
+                                             interim_results=True)
+
+###############################################
+#### Prepare the for recording using Pyaudio ##
+###############################################
+
+# Variables for recording the speech
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# define callback for pyaudio to store the recording in queue
+def pyaudio_callback(in_data, frame_count, time_info, status):
+    try:
+        q.put(in_data)
+    except Full:
+        pass # discard
+    return (None, pyaudio.paContinue)
+
+# instantiate pyaudio
+audio = pyaudio.PyAudio()
+
+# open stream using callback
+stream = audio.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK,
+    stream_callback=pyaudio_callback,
+    start=False
+)
+
+#########################################################################
+#### Start the recording and start service to recognize the stream ######
+#########################################################################
+
+print("Enter CTRL+C to end recording...")
+stream.start_stream()
+
+try:
+    recognize_thread = Thread(target=recognize_using_weboscket, args=())
+    recognize_thread.start()
+
+    while True:
+        pass
+except KeyboardInterrupt:
+    # stop recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    audio_source.completed_recording()
